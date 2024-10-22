5ad52eae60af4ed71af2154e45121e9fe86ab755
alfksp <148875635+alfksp@users.noreply.github.com>
2024-10-22 19:21:59 +0200

    flyway v1 created books table added

---
+++ src/main/resources/db.migration/V1__create_books_table.sql
@@ -0,0 +1,9 @@
+use bookstore;
+CREATE TABLE IF NOT EXISTS books (
+      id BIGINT PRIMARY KEY AUTO_INCREMENT,
+      title VARCHAR(50) NOT NULL,
+      author VARCHAR(50) NOT NULL,
+      year INTEGER CHECK (year > 1900),
+      price DECIMAL(10,2),
+      booksAvailable INTEGER DEFAULT 0
+);
