1a85cbcbddc3012f20583f9d661b3ce7296af387
alfksp <148875635+alfksp@users.noreply.github.com>
2024-10-22 19:31:35 +0200

    flyway v2 created insert books added

---
+++ src/main/resources/db.migration/V2__insert_books.sql
@@ -0,0 +1,8 @@
+use bookstore;
+insert into books (title, author, year, price, books_available)
+values
+    ('Harry Potter and the Philosophers Stone', 'J.K. Rowling', 1997, 26.15, 10),
+    ('Master and Margarita', 'Mikhail Bulgakov', 1967, 39.50, 5),
+    ('East of Eden', 'John Steinbeck', 1952, 41.99, 5),
+    ('Animal Farm', 'George Orwell', 1945, 18.99, 10),
+    ('Men Like Gods', 'Herbert George Wells', 1923, 15.20, 10);
