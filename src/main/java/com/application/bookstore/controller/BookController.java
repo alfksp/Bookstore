da5cafe2965ef7e46096578253bef4c440b6e959
alfksp <148875635+alfksp@users.noreply.github.com>
2024-10-22 19:53:36 +0200

    BookController added

---
+++ src/main/java/com/application/bookstore/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.application.bookstore.controller;
+
+import com.application.bookstore.model.Book;
+import com.application.bookstore.service.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    @GetMapping("/display-books")
+    public String displayAllBooks(Model model) {
+        List<Book> books = bookService.findAll();
+        System.out.println(books.size());
+        model.addAttribute("books", books);
+
+        return "display-books";
+    }
+}
+
