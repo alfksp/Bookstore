0c0c67c914eeaa543bc328cf4329280fb00eeba0
alfksp <148875635+alfksp@users.noreply.github.com>
2024-10-28 12:28:52 +0100

    add-book endpoint in BookController added

--- src/main/java/com/application/bookstore/controller/BookController.java
+++ src/main/java/com/application/bookstore/controller/BookController.java
@@ -5,7 +5,9 @@ import com.application.bookstore.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -34,6 +36,12 @@ public class BookController {
     public String addBookForm(Model model){
         model.addAttribute("book", new Book());
         return "add-book-form";
+    }
+
+    @PostMapping("/add-book")
+    public String addBook(@ModelAttribute("book") Book book){
+        bookService.add(book);
+        return "redirect:display-books";
     }
 
 
