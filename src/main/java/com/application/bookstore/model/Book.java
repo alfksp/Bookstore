3891fdd690339b5f59f3841d12c77595946fbfb2
alfksp <148875635+alfksp@users.noreply.github.com>
2024-12-17 14:01:46 +0100

    BookServiceTest.java class added

--- src/main/java/com/application/bookstore/model/Book.java
+++ src/main/java/com/application/bookstore/model/Book.java
@@ -1,11 +1,15 @@
 package com.application.bookstore.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "BOOKS")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
     @Id
     @Column(name = "ID")
@@ -18,7 +22,7 @@ public class Book {
     @Column(name = "YEAR")
     private int year;
     @Column(name = "PRICE")
-    private double price = 0.25;
+    private double price;
     @Column(name = "BOOKS_AVAILABLE")
     private int booksAvailable;
 }
---
+++ src/test/java/com/application/bookstore/service/BookServiceTest.java
@@ -0,0 +1,109 @@
+package com.application.bookstore.service;
+
+import com.application.bookstore.model.Book;
+import com.application.bookstore.repository.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+
+    @Mock
+    BookRepository bookRepositoryMock;
+    @InjectMocks
+    BookService bookService;
+
+
+//    @BeforeEach
+//    void setUp() {
+//        // Setup test data for books
+//        book = new Book(1L, "Book 1", "Author 1", 2020, 19.99, 10);
+//    }
+
+    @Test
+    void checkIfAllBooksAreFound() {
+        //given
+        List<Book> books = List.of(new Book(), new Book());
+        when(bookRepositoryMock.findAll()).thenReturn(books);
+
+        //when
+        List<Book> result = bookService.findAll();
+
+        //then
+       assertEquals(books, result);
+       assertThat(result).hasSize(2);
+       assertNotNull(result);
+       assertEquals(2, result.size());
+    }
+
+    @Test
+    void checkIfFoundById() {
+
+        //given
+        Book book = new Book();
+        when(bookRepositoryMock.findById(1L)).thenReturn(book);
+
+        //when
+        Book result = bookService.findById(1L);
+
+        //then
+        assertEquals(book, result);
+        assertThat(result).isNotNull();
+        assertThat(result).isSameAs(book);
+    }
+
+    @Test
+    void checkIfAdded() {
+        //given
+        Book book = new Book();
+        when(bookRepositoryMock.save(book)).thenReturn(book);
+
+        //when
+        bookService.add(book);
+
+        //then
+        verify(bookRepositoryMock).save(book);
+        assertNotNull(book);
+
+    }
+
+    @Test
+    void checkIfDeleted() {
+        //given
+        Book book = new Book();
+
+        //when
+        bookService.delete(book);
+
+        //then
+        verify(bookRepositoryMock).delete(book);
+    }
+
+    @Test
+    void checkIfEdited() {
+        //given
+        Book book = new Book(1L, "Author", "Title", 1990, 12.20, 3);
+
+        when(bookRepositoryMock.save(book)).thenReturn(book);
+
+        //when
+        Book result = bookService.edit(book);
+
+        //then
+        assertEquals(book, result);
+
+    }
+}
