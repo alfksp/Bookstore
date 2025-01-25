78c6773899f9042da61fdab25232928add9e3199
alfksp <148875635+alfksp@users.noreply.github.com>
2025-01-25 14:30:38 +0100

    new constants added to CartControllerConstants

--- src/main/java/com/application/bookstore/constants/CartControllerConstants.java
+++ src/main/java/com/application/bookstore/constants/CartControllerConstants.java
@@ -9,4 +9,7 @@ public class CartControllerConstants {
     public static final String DISPLAY_CARTS = "display-carts";
     public static final String BOOKS_NOT_FOUND = "No books were found in this cart";
     public static final String BOOK_DELETED_FROM_CART ="Book was successfully deleted from this cart";
+    public static final String ALL_FIELDS_REQUIRED_CART = "All fields must be filled";
+    public static final String BOOK_ADDED_TO_CART = "Book was added to the cart";
+    public static final String BOOK_NOT_ADDED_TO_CART = "Book wasn't added to the cart";
 }
