bd73134d443a1d2f692431a1dcd15920664b2568
alfksp <148875635+alfksp@users.noreply.github.com>
2025-02-12 19:38:41 +0100

    making private CustomerRepository customerRepository field final

--- src/main/java/com/application/bookstore/service/CustomerService.java
+++ src/main/java/com/application/bookstore/service/CustomerService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class CustomerService {
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
     private final PasswordEncoder passwordEncoder;
 
     public CustomerService(CustomerRepository customerRepository) {
